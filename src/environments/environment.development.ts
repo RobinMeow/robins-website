7feeba4d97b1fdd04704db5a875dc3fc06c856dc
robinmeow97@gmail.com <111490292+RobinMeow@users.noreply.github.com>
2023-11-29 19:55:41 +0100

    add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  useEmulatorFirestore: true,
+  firebase: {
+    projectId: 'demo-project',
+    appId: '1:1003439480657:web:e34dfbaca85d512f143d70',
+    storageBucket: 'robinshomepage-c5bca.appspot.com',
+    apiKey: 'AIzaSyArCJgPGpx6i5W7feFWNMBzek8NvKvp6L0',
+    authDomain: 'robinshomepage-c5bca.firebaseapp.com',
+    messagingSenderId: '1003439480657',
+  },};
